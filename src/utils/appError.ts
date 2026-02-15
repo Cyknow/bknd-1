0994c7a231753501385f7f2ccaceeb5db22d3291
Prince <ctsnlgroup@gmail.com>
2026-02-15 05:30:09 +0100

    like now

--- src/utils/appError.ts
+++ src/utils/appError.ts
@@ -10,33 +10,5 @@ export default class AppError extends Error {
     this.isOperational = true; // IMPORTANT
 
     Error.captureStackTrace(this, this.constructor);
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-// class AppError extends Error {
-//   public statusCode: number;
-//   public status: string;
-//   public isOperational: boolean;
-
-//   constructor(message: string, statusCode: number) {
-//     super(message);
-
-//     this.statusCode = statusCode;
-//     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-//     this.isOperational = true; // Mark as expected error
-
-//     Error.captureStackTrace(this, this.constructor);
-//   }
-// }
-
-// export default AppError;
+  };
+};
