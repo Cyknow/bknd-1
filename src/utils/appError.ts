221d85953e3be9d093c1dabd0268a597c517c4ba
Prince <ctsnlgroup@gmail.com>
2026-02-17 00:04:42 +0100

    Latest

--- src/utils/appError.ts
+++ src/utils/appError.ts
@@ -9,6 +9,9 @@ export default class AppError extends Error {
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true; // IMPORTANT
 
-    Error.captureStackTrace(this, this.constructor);
+    // Error.captureStackTrace(this, this.constructor);
+    if (Error.captureStackTrace) {
+  Error.captureStackTrace(this, this.constructor);
+}
   };
 };
